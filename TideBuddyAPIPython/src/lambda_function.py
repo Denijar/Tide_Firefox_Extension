23235bbe5a98898ede87c2733fe533284e4583a1
Denise PC <43567020+Denijar@users.noreply.github.com>
2020-04-16 16:47:57 +1200

    created basic Makefile for installing chromedriver and headless chromium, added first attempt for scraping tide data

--- TideBuddyAPIPython/src/lambda_function.py
+++ TideBuddyAPIPython/src/lambda_function.py
@@ -1,7 +1,57 @@
 import json
 
+from selenium import webdriver
+import os
+
+url = "https://www.metservice.com/marine/regions/east-auckland/tides/locations/"
+
 def lambda_handler(event, context):
+
+    if( 'queryStringParameters' in event and 'city' in event['queryStringParameters']):
+
+        city = event["queryStringParameters"]["city"]
+        queryURL = url + city
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--window-size=1280x1696')
+        chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+        chrome_options.add_argument('--hide-scrollbars')
+        chrome_options.add_argument('--enable-logging')
+        chrome_options.add_argument('--log-level=0')
+        chrome_options.add_argument('--v=99')
+        chrome_options.add_argument('--single-process')
+        chrome_options.add_argument('--data-path=/tmp/data-path')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--homedir=/tmp')
+        chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+        chrome_options.add_argument(
+            'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36')
+        chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        driver.get(queryURL)
+        # elemCopiedPath = driver.find_element_by_xpath('/html/body/main/div[2]/div[3]/div[2]/section/div[2]/div[2]/div[2]/div/div[2]')
+        # HTML1 = elemCopiedPath.get_attribute('innerHTML')
+
+        elemRelativePath = driver.find_element_by_xpath('//div[@data-module-name="tides-timetable"]')
+        HTML2 = elemRelativePath.get_attribute('innerHTML')
+
+        body = HTML2
+        statusCode = 200
+
+        driver.quit()
+
+
+    else:
+        statusCode = 400 # Bad request error code
+        body = "Incorrect query string parameters were provided"
+
+
     return {
-        "statusCode": 200,
-        "body": json.dumps("Tide data")
+        "statusCode": statusCode,
+        "body": json.dumps(body)
     }
